c9ff415ad23e762467cb8dd9c91f056797559e76
keperkjr <keperkjr@yahoo.com>
2022-01-03 14:24:44 -0800

    Added database context classes

---
+++ CS-UrlRedirect-Pages/Data/DatabaseDBContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using CS_UrlRedirect_Pages.Models;
+
+#nullable disable
+
+namespace CS_UrlRedirect_Pages.Data
+{
+    public partial class DatabaseDBContext : DbContext
+    {
+        public DatabaseDBContext()
+        {
+        }
+
+        public DatabaseDBContext(DbContextOptions<DatabaseDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Redirect> Redirects { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Redirect>(entity =>
+            {
+                entity.HasIndex(e => e.ShortCode, "U_SHORTCODE")
+                    .IsUnique();
+
+                entity.Property(e => e.NumVisits).HasColumnName("numVisits");
+
+                entity.Property(e => e.ShortCode)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("shortCode");
+
+                entity.Property(e => e.Url)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .HasColumnName("url");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ CS-UrlRedirect-Pages/Models/Redirect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace CS_UrlRedirect_Pages.Models
+{
+    public partial class Redirect
+    {
+        public int Id { get; set; }
+        public string ShortCode { get; set; }
+        public string Url { get; set; }
+        public int NumVisits { get; set; }
+    }
+}
