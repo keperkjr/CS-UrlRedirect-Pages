434b7b898b95622784a6ba8f82c7ad02ebc7d649
keperkjr <keperkjr@yahoo.com>
2022-01-03 16:55:57 -0800

    Redirect form working

--- CS-UrlRedirect-Pages/Pages/Index.cshtml.cs
+++ CS-UrlRedirect-Pages/Pages/Index.cshtml.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Utils;
+using System.Diagnostics;
 
 namespace CS_UrlRedirect_Pages.Pages
 {
@@ -48,10 +49,81 @@ namespace CS_UrlRedirect_Pages.Pages
             await ShowIndex();
         }
 
-        public async Task<IActionResult> OnPostCreateAsync()
+        public async Task<IActionResult> OnPostCreateAsync([Bind("Id,ShortCode,Url,action")] RedirectViewModel redirectVM)
         {
-            await Task.Delay(500);
-            return RedirectToPage();
+            if (string.IsNullOrWhiteSpace(redirectVM.ShortCode))
+            {
+                ModelState.AddModelError(nameof(redirectVM.ShortCode), "A short code is required");
+            }
+            else
+            {
+                redirectVM.ShortCode = redirectVM.ShortCode.Trim();
+                if (redirectVM.action == RedirectViewModel.Action.Create && await _redirectService.ExistsAsync(redirectVM.ShortCode))
+                {
+                    ModelState.AddModelError(nameof(redirectVM.ShortCode), "The following short code is unavailable and cannot be used");
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(redirectVM.Url))
+            {
+                ModelState.AddModelError(nameof(redirectVM.Url), "A redirect url is required");
+            }
+            else
+            {
+                redirectVM.Url = redirectVM.Url.Trim();
+                if (!Http.IsValidURL(redirectVM.Url))
+                {
+                    ModelState.AddModelError(nameof(redirectVM.Url), "A valid destination url is required");
+                }
+            }
+
+            if (ModelState.IsValid)
+            {
+                switch (redirectVM.action)
+                {
+                    case RedirectViewModel.Action.Create:
+                        await CreateEntry(redirectVM);
+                        break;
+                    case RedirectViewModel.Action.Update:
+                        try
+                        {
+                            await UpdateEntry(redirectVM);
+                        }
+                        catch (DbUpdateConcurrencyException)
+                        {
+                            if (await _redirectService.ExistsAsync(redirectVM.Id))
+                            {
+                                throw;
+                            }
+                            return NotFound();
+                        }
+                        break;
+                }
+                return new JsonResult(new { redirectTo = Url.Action(nameof(Index)) });
+            }
+            else
+            {
+                var errors = ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .Select(x => new { x.Key, x.Value.Errors })
+                    .ToList();
+
+                Debug.Print("");
+            }
+            return Partial("_RedirectForm", redirectVM);
+        }
+
+        public async Task CreateEntry(RedirectViewModel redirectVM)
+        {
+            var redirect = new Redirect();
+            redirectVM.CopyPropsTo(ref redirect);
+            await _redirectService.AddAsync(redirect);
+        }
+
+        // https://docs.microsoft.com/en-us/ef/core/saving/disconnected-entities
+        public async Task UpdateEntry(RedirectViewModel redirectVM)
+        {
+            await _redirectService.UpdateAsync(redirectVM.Id, redirectVM);
         }
     }
 }
